7dab4d96136400a817c7e25bd0c4d8545a69d570
Faishal-Monir <businessinfo.server1@gmail.com>
2025-08-09 16:33:24 +0600

    quality fixes to run the program from a single end point automatically

--- server.bat
+++ server.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "D:\CSE-471 Project"
-start "Backend" cmd /k "cd /d Thesis-Management-System\backend && node index.js"
-start "Frontend" cmd /k "cd /d Thesis-Management-System\frontend && npm start"
+start "Backend" cmd /k "cd /d backend && node index.js"
+start "Frontend" cmd /k "cd /d frontend && npm start"
 echo Launched Backend and Frontend terminals.
 pause
+endlocal
