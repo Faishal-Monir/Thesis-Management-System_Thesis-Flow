76f516d1b5516d39b43dec306c412302500c203d
Nafiz Khan <nafizk368@gmail.com>
2025-08-11 06:36:30 +0600

    ..

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5005'; // Use environment variable or fallback to localhost
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5005'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
